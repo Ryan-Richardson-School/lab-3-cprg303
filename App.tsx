d4b05e0989f73159c88641089fe9b88068bcd3b7
AnagRoy <anagha.roy@edu.sait.ca>
2026-03-10 18:53:09 -0600

    Completed SignUp screen with full Zod validation and UI

--- App.tsx
+++ App.tsx
@@ -1,17 +1,10 @@
-// import React from "react";
-// import EmployeeForm from "./src/screens/EmployeeForm";
-
-// export default function App() {
-//   return <EmployeeForm />;
-// }
-
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import EmployeeForm from "./src/screens/EmployeeForm";
 import SignIn from "./src/screens/SignIn";
-//import SignUp from "./src/screens/SignUp";
+import SignUp from "./src/screens/SignUp";
 
 const Stack = createNativeStackNavigator();
 
@@ -21,7 +14,7 @@ export default function App() {
       <Stack.Navigator initialRouteName="EmployeeForm">
         <Stack.Screen name="EmployeeForm" component={EmployeeForm} />
         <Stack.Screen name="SignIn" component={SignIn} />
-        {/* <Stack.Screen name="SignUp" component={SignUp} /> */}
+        <Stack.Screen name="SignUp" component={SignUp} />
       </Stack.Navigator>
     </NavigationContainer>
   );
